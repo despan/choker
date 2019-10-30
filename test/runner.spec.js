cb5d0f042e69334d1e77364b172cf4fc10e27d96
Mushegh Antaranyan <mushegh.antaranyan@gmail.com>
2019-10-30 09:16:56 +0400

    test: add execution timer
    Debug estimated and elapsed (pretty close here) using `process.hrtime`
    
        Run 50 with limit of 10
          ideally should take 5 s
          took 5.05 s

--- test/runner.spec.js
+++ test/runner.spec.js
@@ -4,6 +4,7 @@ import R from 'ramda'
 
 import { HTTPError } from 'got'
 
+import Debug from './helpers/debug'
 import { createServer } from './helpers/server'
 
 import runner from '../src/runner'
@@ -18,6 +19,8 @@ const TOTAL = 50
 /*
  * Helpers
  */
+
+const debug = Debug('sfc:runner:test')
 
 const x3 = x => x * 3
 
@@ -47,6 +50,17 @@ test.serial('limits', async t => {
 
   const run = limit => runner({ url, limit }, numbers)
 
+  // ok case
+  debug('Run %d with limit of %d', TOTAL, LIMIT)
+  debug('  ideally should take %d s', TOTAL / LIMIT)
+  const startOk = process.hrtime()
+
   await t.notThrowsAsync(() => run(LIMIT), 'below limit')
+
+  const endOk = process.hrtime(startOk)
+  debug('  took %h', endOk)
+
+  // err case
+
   await t.throwsAsync(() => run(x3(LIMIT)), HTTPError, 'above limit')
 })
